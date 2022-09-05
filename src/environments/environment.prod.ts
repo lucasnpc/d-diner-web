c271bce0b41ea33b4b7f61c4d7f2a72ee8a3ff92
lucasnpc <lucasnpc2@outlook.com>
2022-09-05 20:38:16 -0300

    Adding firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  firebase: {
+    projectId: 'd-diner',
+    appId: '1:592852065637:web:470ed702aa4080e300d58c',
+    databaseURL: 'https://d-diner-default-rtdb.firebaseio.com',
+    storageBucket: 'd-diner.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAHdsr45sjl4gSzPwxAvhg8vI3wK0CNGUE',
+    authDomain: 'd-diner.firebaseapp.com',
+    messagingSenderId: '592852065637',
+    measurementId: 'G-Y4DZKEQFKY',
+  },
+  production: true,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,18 @@
 
 export const environment = {
   production: false,
-  url: "http://localhost:8000/"
+  url: "http://localhost:8000/",
+  firebase: {
+    projectId: 'd-diner',
+    appId: '1:592852065637:web:470ed702aa4080e300d58c',
+    databaseURL: 'https://d-diner-default-rtdb.firebaseio.com',
+    storageBucket: 'd-diner.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAHdsr45sjl4gSzPwxAvhg8vI3wK0CNGUE',
+    authDomain: 'd-diner.firebaseapp.com',
+    messagingSenderId: '592852065637',
+    measurementId: 'G-Y4DZKEQFKY',
+  }
 };
 
 /*
